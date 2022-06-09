688f2f95beb5f613505dd15887c86deeed6d2b84
marcegozz <marcellogozzerino@hotmail.com>
2022-06-09 15:13:45 -0300

    Update models.py

--- Aplicacion/models.py
+++ Aplicacion/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Superhumano (models.Model):
+        nombre = models.CharField(max_length=40)
+        edad = models.IntegerField()
+
+class Organizacion(models.Model):
+    nombre = models.CharField(max_length=40)
+    cantIntegrantes = models.IntegerField()
+
+class Avistamiento(models.Model):
+    superhumano = models.CharField(max_length=40)
+    descripcionLugar = models.CharField(max_length=100)
+    
