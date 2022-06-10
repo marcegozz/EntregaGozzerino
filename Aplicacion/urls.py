6fe12974107903631d5c621d7982508385555d0d
marcegozz <marcellogozzerino@hotmail.com>
2022-06-09 22:02:25 -0300

    Urls y Views

--- Aplicacion/urls.py
+++ Aplicacion/urls.py
@@ -1,2 +1,11 @@
-from django.urls import path
+from django.urls import URLPattern, path
 from Aplicacion import views
+
+URLPattern = [
+    path('', views.inicio, name="index"),
+    path('nuevoAvistamiento', views.nuevoAvistamiento, name = "nuevaAparicion"),
+    path('buscarAvistamiento', views.buscarAvistamiento, name = "buscarAparicion"),
+    path('nuevoHeroe', views.nuevoHeroe, name = "nuevoHeroe"),
+    path('nuevaOrganizacion', views.nuevaOrganizacion, name = "nuevaOrganizacion"),
+    path('buscar/', views.buscar)
+]
--- Aplicacion/views.py
+++ Aplicacion/views.py
@@ -4,57 +4,57 @@ from Aplicacion.forms import nuevoAvistamiento, nuevaOrganización, nuevoHeroe
 from Aplicacion.models import Superhumano, Avistamiento, Organizacion
 
 def inicio(request):
-    return render(request, "-")
+    return render(request, "Aplicacion/index.html")
 
 def nuevoAvistamiento(request):
     if request.method == 'POST':
-        miFormulario = NuevaAparicion(request.POST)
+        miFormulario = nuevoAvistamiento(request.POST)
 
         print(miFormulario)
 
         if miFormulario.is_valid:
             informacion = miFormulario.cleaned_data
-            aparicion = Aparicion(descripcionLugar = informacion['descripcionLugar'], superhumano=informacion['superhumano'])
+            aparicion = Avistamiento(descripcionLugar = informacion['descripcionLugar'], superhumano=informacion['superhumano'])
             aparicion.save()
-            return render(request, "-")
+            return render(request, "Aplicacion/index.html")
     else:
-            miFormulario = NuevaAparicion()
-    return render(request, "-", {"miFormulario":miFormulario})
+            miFormulario = nuevoAvistamiento()
+    return render(request, "Alicacion/nuevoAvistamiento.html", {"miFormulario":miFormulario})
 
 def nuevoHeroe(request):
     if request.method == 'POST':
-        miFormulario = NuevoHeroe(request.POST)
+        miFormulario = nuevoHeroe(request.POST)
         print(miFormulario)
         if miFormulario.is_valid:
             informacion = miFormulario.cleaned_data
             heroe = Superhumano(nombre = informacion['nombre'], nivelDePoder=informacion['poder'])
             heroe.save()
-            return render(request, "-")
+            return render(request, "Aplicacion/index.html")
     else:
-            miFormulario = NuevoHeroe()
-    return render(request, "-", {"miFormulario":miFormulario})
+            miFormulario = nuevoHeroe()
+    return render(request, "Aplicacion/nuevoHeroe.html", {"miFormulario":miFormulario})
 
 def nuevaOrganizacion(request):
     if request.method == 'POST':
-        miFormulario = NuevaOrganizacion(request.POST)
+        miFormulario = nuevaOrganizacion(request.POST)
         print(miFormulario)
         if miFormulario.is_valid:
             informacion = miFormulario.cleaned_data
             organizacion = Organizacion(nombre = informacion['nombre'], cantIntegrantes=informacion['cantIntegrantes'])
             organizacion.save()
-            return render(request, "-")
+            return render(request, "Aplicacion/index.html")
     else:
-            miFormulario = NuevaOrganizacion()
-    return render(request, "-", {"miFormulario":miFormulario})
+            miFormulario = nuevaOrganizacion()
+    return render(request, "Aplicacion/index.html", {"miFormulario":miFormulario})
 
-def buscarAparicion(request):
-    return render(request, "-")
+def buscarAvistamiento(request):
+    return render(request, "Aplicacion/buscarAvistamiento.html")
 
 def buscar(request):
     if request.GET["superhumano"]:
         heroe = request.GET["superhumano"]
-        apariciones = Aparicion.objects.filter(superhumano__icontains=heroe)
-        return render(request, "-", {"superhumano":heroe,"apariciones":apariciones})
+        apariciones = Avistamiento.objects.filter(superhumano__icontains=heroe)
+        return render(request, "Aplicacion/resultadoBusquedaAvistamiento.html", {"superhumano":heroe,"apariciones":apariciones})
     else:
         respuesta = "No enviaste datos"
         return HttpResponse(respuesta)
